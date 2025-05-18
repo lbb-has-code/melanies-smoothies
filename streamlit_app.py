c98f28cf547114f45f8e1eb733b9334335eab270
lbb-has-code <lonnye@gmail.com>
2025-05-18 21:32:02 +0800

    Update streamlit_app.py
    replacing snowpark frame to panda frame

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,6 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
 
-
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
@@ -18,7 +17,11 @@ st.write('The name on your Smoothie will be:', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'), col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+# convert Snowpark dataframe to panda dataframe
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 ingredients_list = st.multiselect(
