f9fc58904488ce5180aa8b601e16f6cc56a46913
lbb-has-code <lonnye@gmail.com>
2025-05-18 20:49:28 +0800

    Update streamlit_app.py
    moving smoothifroot info to bottom of screen

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,6 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response)
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
 # Write directly to the app
@@ -33,6 +30,10 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        #st.text(smoothiefroot_response)
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
     
     #st.write(ingredients_string)
 
